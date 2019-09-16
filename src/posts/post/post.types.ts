d9143eb2f0c1ffd9d0c83df812237a1aa1e92af9
Petr Juna <petr.juna@gmail.com>
2019-09-16 18:39:40 +0200

    chore(model): lib-model changed (undone)

--- src/posts/post/post.types.ts
+++ src/posts/post/post.types.ts
@@ -1,4 +1,4 @@
 export { PostModel as PostMongo  } from '@pyxismedia/lib-model/post/post.model';
 export { CreatePostDto } from '@pyxismedia/lib-model/post/create-post.dto';
-export { Section } from '@pyxismedia/lib-model/post/section.model';
+export { SectionModel as Section } from '@pyxismedia/lib-model/post/section.model';
 export { PostStateEnum as PostState } from '@pyxismedia/lib-model/post/post-state.enum';
