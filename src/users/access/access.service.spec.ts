9374391042c546a87473f72d601e8d46da03d04b
Petr Juna <petr.juna@gmail.com>
2019-11-16 16:51:49 +0100

    fix(access): restricted access added

--- src/users/access/access.service.spec.ts
+++ src/users/access/access.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccessService } from './access.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { ACCESS_MODEL } from '@pyxismedia/lib-model';
+
+const accessModel = {
+  title: 'Title',
+};
 
 describe('AccessService', () => {
   let service: AccessService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccessService],
+      providers: [
+        AccessService,
+        {
+          provide: getModelToken(ACCESS_MODEL),
+          useValue: accessModel,
+        },
+      ],
     }).compile();
 
     service = module.get<AccessService>(AccessService);
