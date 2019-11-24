f4503f67da108d6038dab54265874003f556965b
Petr Juna <petr.juna@gmail.com>
2019-11-24 10:15:50 +0100

    chore(post): error handling

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ValidationPipe } from './validation.pipe';
 import { ParseObjectIdPipe } from './parse-object-id.pipe';
 import { ParseNumberPipe } from './parse-number.pipe';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
+  imports: [LoggerModule],
   providers: [ValidationPipe, ParseObjectIdPipe, ParseNumberPipe],
   exports: [ValidationPipe, ParseObjectIdPipe, ParseNumberPipe],
 })
